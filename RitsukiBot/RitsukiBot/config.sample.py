e24a16a433ada4be2a64839e48487e1037849abc
Lyn DK <d1366160279@gmail.com>
2021-09-05 10:38:50 +0800

    Delete RitsukiBot directory

--- RitsukiBot/RitsukiBot/config.sample.py
+++
@@ -1,8 +0,0 @@
-from nonebot.default_config import *
-
-
-HOST = '0.0.0.0'
-PORT = 8080
-
-SUPERUSERS = {}
-COMMAND_START = {"/"}
--- RitsukiBot/RitsukiBot/res/plugins/rss/data_resource.sample.py
+++
@@ -1,2 +0,0 @@
-rss_list = ['gf-cn/news', 'epicgames/freegames', 'bilibili/user/dynamic/404145357']
-group_list = []
