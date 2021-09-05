e47599fd50e7782327af486548821d91e5c3d19a
Lyn DK <d1366160279@gmail.com>
2021-09-05 11:16:11 +0800

    Update function.py

--- RitsukiBot/res/plugins/water_lv/function.py
+++ RitsukiBot/res/plugins/water_lv/function.py
@@ -7,7 +7,7 @@ async def get_water_lv(ShuiWei):
     url = "https://tianqiapi.com/api?version=v6&appid=93561677&appsecret=xTW8n2Al&cityid=101200201"
     r = requests.get(url)
     jsons = json.loads(r.text)
-    wb = openpyxl.load_workbook("res/docs/水位记录.xlsx")
+    wb = openpyxl.load_workbook("res/docs/water_lv_log.xlsx")
     sh0 = wb.worksheets[0]
     sh0.cell(sh0.max_row+1, 1).value = jsons["update_time"]
     sh0.cell(sh0.max_row, 2).value = ShuiWei
@@ -20,7 +20,7 @@ async def get_water_lv(ShuiWei):
         flag = "水位下降"+str(int(abs(minus*100)))+"cm\n"
     else:
         flag = "无变化\n"
-    wb.save("res/docs/水位记录.xlsx")
+    wb.save("res/docs/water_lv_log.xlsx")
     result = "测量时间："+jsons["update_time"]+"\n"+"目前水位："+ShuiWei+"m\n"+"较昨日"+yesterday_time+" 测量 "+str(yesterday_ShuiWei)+"m\n"+flag+"当前天气："+jsons["wea"]+"\n"+"风力："+jsons["win"]+jsons["win_speed"]
     wb.close()
     return result
