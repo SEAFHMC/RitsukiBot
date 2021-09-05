61f45c292e43c559effa3df9d6fd40d31885bf76
Lyn DK <d1366160279@gmail.com>
2021-09-05 11:15:57 +0800

    Update __init__.py

--- RitsukiBot/res/plugins/water_lv/__init__.py
+++ RitsukiBot/res/plugins/water_lv/__init__.py
@@ -1,26 +1,24 @@
-import json
-import requests
-import openpyxl
+from nonebot import on_command, CommandSession
+from .function import get_water_lv
+__plugin_name__ = '水位'
+__plugin_usage__ = r"""生成每日水位监测报告
+用法：/水位 [当前测量水位]"""
 
 
-async def get_water_lv(ShuiWei):
-    url = "https://tianqiapi.com/api?version=v6&appid=93561677&appsecret=xTW8n2Al&cityid=101200201"
-    r = requests.get(url)
-    jsons = json.loads(r.text)
-    wb = openpyxl.load_workbook("res/docs/water_lv_log.xlsx")
-    sh0 = wb.worksheets[0]
-    sh0.cell(sh0.max_row+1, 1).value = jsons["update_time"]
-    sh0.cell(sh0.max_row, 2).value = ShuiWei
-    yesterday_time = sh0.cell(sh0.max_row-1, 1).value[11:]
-    yesterday_ShuiWei = round(float(sh0.cell(sh0.max_row-1, 2).value), 2)
-    minus = round(round(float(ShuiWei), 2)-yesterday_ShuiWei, 2)
-    if float(minus) > 0:
-        flag = "水位上涨"+str(int(abs(minus*100)))+"cm\n"
-    elif float(minus) < 0:
-        flag = "水位下降"+str(int(abs(minus*100)))+"cm\n"
-    else:
-        flag = "无变化\n"
-    wb.save("res/docs/water_lv_log.xlsx")
-    result = "测量时间："+jsons["update_time"]+"\n"+"目前水位："+ShuiWei+"m\n"+"较昨日"+yesterday_time+" 测量 "+str(yesterday_ShuiWei)+"m\n"+flag+"当前天气："+jsons["wea"]+"\n"+"风力："+jsons["win"]+jsons["win_speed"]
-    wb.close()
-    return result
+@on_command('水位')
+async def water_lv(session: CommandSession):
+    shuiwei = session.get('shuiwei', prompt='请输入目前水位：')
+    shuiwei_result = await get_water_lv(shuiwei)
+    await session.send(shuiwei_result)
+
+
+@water_lv.args_parser
+async def _(session: CommandSession):
+    stripped_arg = session.current_arg_text.strip()
+    if session.is_first_run:
+        if stripped_arg:
+            session.state['shuiwei'] = stripped_arg
+        return
+    if not stripped_arg:
+        session.pause('水位不能为空，请重新输入')
+    session.state[session.current_key] = stripped_arg
