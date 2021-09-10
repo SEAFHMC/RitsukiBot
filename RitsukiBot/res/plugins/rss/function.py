177588396c5e950d574b48b14a7bc0beaf48e8e6
Lyn DK <d1366160279@gmail.com>
2021-09-10 09:21:52 +0800

    Update function.py

--- RitsukiBot/res/plugins/rss/function.py
+++ RitsukiBot/res/plugins/rss/function.py
@@ -23,7 +23,7 @@ async def strip_deco(str):
 
 
 async def check_update(rss_id):
-    with open('./res/plugins/rss/'+rss_id.replace('/', '-')+'.ujson', 'r', encoding='UTF-8') as f:
+    with open('./res/plugins/rss/'+rss_id.replace('/', '-')+'.json', 'r', encoding='UTF-8') as f:
         url = r'http://107.182.17.60:1200/'+rss_id
         parser = feedparser.parse(url)
         ujsons = ujson.dumps(parser.entries[0], indent=2, ensure_ascii=False)
@@ -34,7 +34,7 @@ async def check_update(rss_id):
 
 
 async def get_update(rss_id):
-    with open('./res/plugins/rss/'+rss_id.replace('/', '-')+'.ujson', 'w+', encoding='UTF-8') as f:
+    with open('./res/plugins/rss/'+rss_id.replace('/', '-')+'.json', 'w+', encoding='UTF-8') as f:
         url = r'http://107.182.17.60:1200/'+rss_id
         parser = feedparser.parse(url)
         ujsons = ujson.dumps(parser.entries[0], indent=2, ensure_ascii=False)
