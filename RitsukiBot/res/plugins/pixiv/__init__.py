20382445d96fb65dc241afdc145c191bc8ff3464
Lyn DK <d1366160279@gmail.com>
2021-09-16 21:01:26 +0800

    Update __init__.py

--- RitsukiBot/res/plugins/pixiv/__init__.py
+++ RitsukiBot/res/plugins/pixiv/__init__.py
@@ -1,11 +1,11 @@
 from nonebot import on_command, CommandSession, MessageSegment
-import ujson
-import requests
+from .function import get_rank, get_img_url
 __plugin_name__ = 'pixiv'
 __plugin_usage__ = r"""1、获取Pixiv榜单并返回图片
 用法：/pixivrank [key_word]
 当前支持的key_word有：
-daily, weekly, monthly
+daily, weekly, monthly,
+rookie,
 可在key_word后附加_r18
 
 2、根据pid返回图片
@@ -22,14 +22,8 @@ async def pixivid(session: CommandSession):
 
 @on_command('pixivrank', only_to_me=False)
 async def pixivrank(session: CommandSession):
-    rank = session.current_arg_text
-    url = "https://api.loli.st/pixiv/?mode="+rank
-    r = requests.get(url=url, verify=False)
-    ujsons = ujson.loads(r.text)
-    id = ujsons["illust_id"]
-    imgurl_rank = "https://pixiv.cat/"+id+".png"
-    img_r = requests.get(imgurl_rank)
-    if int(img_r.status_code) == 404:
-        imgurl_rank = "https://pixiv.cat/"+id+"-1.png"
+    rank = session.current_arg_text.strip()
+    rank_summary = await get_rank(rank)
+    imgurl_rank = await get_img_url(rank_summary)
     result = MessageSegment.image(imgurl_rank)
     await session.send(result)
