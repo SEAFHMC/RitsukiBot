7a68a1edfbee13a23d2794ba8934e64a7faed507
Lyn DK <d1366160279@gmail.com>
2021-09-19 21:56:44 +0800

    Update radiation.py

--- RitsukiBot/res/plugins/radiation.py
+++ RitsukiBot/res/plugins/radiation.py
@@ -6,11 +6,18 @@ jieba.load_userdict('./res/docs/dict.txt')
 bot = nonebot.get_bot()
 
 
+async def del_str(str, str_list):
+    for i in range(len(str_list)):
+        if str == str_list[i]:
+            del str_list[i]
+            return str_list
+
+
 async def radiation(question):
     try:
         seg_list = jieba.lcut(question, cut_all=True)
-        if seg_list[0] == '文文' and '新闻' not in seg_list:
-            del seg_list[0]
+        if '文文' in seg_list and '新闻' not in seg_list:
+            seg_list = await del_str('文文', seg_list)
             with open('./res/docs/data.json', 'r', encoding='UTF-8') as f:
                 ujsons = ujson.loads(f.read())
                 init = []
