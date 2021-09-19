9a09d56fba32f858e2f2ead7bb50ee18ee202c27
Lyn DK <d1366160279@gmail.com>
2021-09-19 20:48:34 +0800

    Update radiation.py

--- RitsukiBot/res/plugins/radiation.py
+++ RitsukiBot/res/plugins/radiation.py
@@ -6,19 +6,22 @@ bot = nonebot.get_bot()
 
 
 async def radiation(question):
-    seg_list = jieba.lcut(question, cut_all=True)
-    if seg_list[0] == '文文':
-        del seg_list[0]
-        with open('./res/docs/data.json', 'r', encoding='UTF-8') as f:
-            ujsons = ujson.loads(f.read())
-            init = []
-            for i in seg_list:
-                if i in ujsons.keys():
-                    init.append(i)
-            init = choice(init)
-            if init in ujsons.keys():
-                return choice(ujsons[init])
-    else:
+    try:
+        seg_list = jieba.lcut(question, cut_all=True)
+        if seg_list[0] == '文文':
+            del seg_list[0]
+            with open('./res/docs/data.json', 'r', encoding='UTF-8') as f:
+                ujsons = ujson.loads(f.read())
+                init = []
+                for i in seg_list:
+                    if i in ujsons.keys():
+                        init.append(i)
+                init = choice(init)
+                if init in ujsons.keys():
+                    return choice(ujsons[init])
+        else:
+            pass
+    except:
         pass
 
 
@@ -27,4 +30,5 @@ async def chat(context):
     message = context['raw_message'].strip()
     group_id = context['group_id']
     message = await radiation(message)
-    await bot.send_group_msg(group_id=group_id, message=message)
+    if message:
+        await bot.send_group_msg(group_id=group_id, message=message)
