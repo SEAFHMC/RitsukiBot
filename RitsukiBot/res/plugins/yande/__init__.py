774032a306a41f70f2c1f8810a866a1e57c5c1a3
Lyn DK <d1366160279@gmail.com>
2021-12-07 21:10:23 +0800

    Create __init__.py

---
+++ RitsukiBot/res/plugins/yande/__init__.py
@@ -0,0 +1,13 @@
+from nonebot import on_command, CommandSession, MessageSegment
+from .function import acurate_tag
+__plugin_name__ = 'yande'
+__plugin_usage__ = r"""根据tag获取涩图（图源yande.re）
+用法：/yande [tag]"""
+
+
+@on_command('yande', only_to_me=False)
+async def yande(session: CommandSession):
+    tag = session.current_arg_text.strip()
+    url = acurate_tag(tag)
+    result = MessageSegment.image(url)
+    await session.send(result)
