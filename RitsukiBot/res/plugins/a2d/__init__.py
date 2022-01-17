8118f5052ba3c81491389318d32f070375da263b
Lyn DK <d1366160279@gmail.com>
2022-01-17 21:46:25 +0800

    Create __init__.py

---
+++ RitsukiBot/res/plugins/a2d/__init__.py
@@ -0,0 +1,26 @@
+from nonebot import on_command, CommandSession, MessageSegment
+from .function import a2d
+__plugin_name__ = 'a2d'
+__plugin_usage__ = r"""ascii2d搜图
+用法：/yande [tag]"""
+
+
+@on_command('a2d', only_to_me=False)
+async def a2d_search(session: CommandSession):
+    CQCode = session.get('CQCode', prompt='请发送一张图片')
+    search_result = await a2d(CQCode)
+    result_color = (
+        "色合搜索结果: " + '\n' +
+        'title: ' + search_result[0]['title'] + '\n' +
+        'author: ' + search_result[0]['authors'] + '\n' +
+        MessageSegment.image(search_result[0]['thumbnail']) + '\n' +
+        'url: ' + search_result[0]['url'])
+    await session.send(result_color)
+
+    result_bovm = (
+        "特徽搜索结果: " + '\n' +
+        'title: ' + search_result[1]['title'] + '\n' +
+        'author: ' + search_result[1]['authors'] + '\n' +
+        MessageSegment.image(search_result[1]['thumbnail']) + '\n' +
+        'url: ' + search_result[1]['url'])
+    await session.send(result_bovm)
