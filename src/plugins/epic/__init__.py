6b713aca0377d660199f6e829a745dd0923bb30f
Lyn DK <d1366160279@gmail.com>
2022-02-14 20:45:22 +0800

    Update __init__.py

--- src/plugins/epic/__init__.py
+++ src/plugins/epic/__init__.py
@@ -1,11 +1,23 @@
-from nonebot import on_command, get_driver, require, get_bot
+from nonebot import on_command, get_driver, require, get_bot, logger
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent
-from .data_resource import make_msg, new_promotion
+from .data_resource import make_msg, new_promotion, path, url
+import os
+from utils.utils import httpx_request
 
+driver = get_driver()
 groups = get_driver().config.epic_subscribe_group
 users = get_driver().config.epic_subscribe_user
 
 epic = on_command("epic", priority=6)
+
+
+@driver.on_startup
+async def first_run():
+    if not os.path.exists(path+"/epic.json"):
+        logger.info('正在创建epic.json')
+        content = await httpx_request(url)
+        with open(path+'/epic.json', 'w+', encoding='UTF-8') as f:
+            f.write(content)
 
 
 @epic.handle()
@@ -17,10 +29,10 @@ scheduler = require("nonebot_plugin_apscheduler").scheduler
 
 
 # 每两个小时检查一次更新
-@scheduler.scheduled_job("cron", hour="*/2")
+@scheduler.scheduled_job("cron", hour="*/2", id="Every_TwoHours")
 async def check_update():
-    bot = get_bot()
     if await new_promotion():
+        bot = get_bot()
         res = await make_msg()
         for group in groups:
             await bot.send_group_msg(group_id=group, message=res)
