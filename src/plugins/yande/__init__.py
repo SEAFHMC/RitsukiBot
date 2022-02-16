2da21d2a5c5b50e9c263767d70a3dd7983a8632a
Lyn DK <d1366160279@gmail.com>
2022-02-16 22:15:23 +0800

    Update __init__.py

--- src/plugins/yande/__init__.py
+++ src/plugins/yande/__init__.py
@@ -11,11 +11,20 @@ __plugin_usage__ = r"""返回一张yande.re的涩图
 用法：/yande [tag]"""
 
 yande = on_command("yande", priority=10)
+danbooru = on_command("danbooru", priority=10)
 
 
 @yande.handle()
-async def handle_first_receive(bot: Bot, event: Event, args: Message = CommandArg()):
+async def yande(bot: Bot, event: Event, args: Message = CommandArg()):
     url = 'https://yande.re/post.json?tags='+str(args).strip()
     img_list = json.loads(await httpx_request(url))
     img_url = choice(img_list)['file_url']
     await yande.finish(MessageSegment.image(img_url))
+
+
+@danbooru.handle()
+async def danbooru(bot: Bot, event: Event, args: Message = CommandArg()):
+    url = 'https://danbooru.donmai.us/posts.json?tags='+str(args).strip()
+    img_list = json.loads(await httpx_request(url))
+    img_url = choice(img_list)['file_url']
+    await danbooru.finish(MessageSegment.image(img_url))
