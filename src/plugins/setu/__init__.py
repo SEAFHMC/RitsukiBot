461861b68d86648f66c3e1e32114978da2a206fb
Lyn DK <d1366160279@gmail.com>
2022-02-16 22:22:05 +0800

    Update and rename src/plugins/yande/__init__.py to src/plugins/setu/__init__.py

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -6,16 +6,14 @@ import ujson as json
 from utils.utils import httpx_request
 from random import choice
 
-__plugin_name__ = 'yande'
-__plugin_usage__ = r"""返回一张yande.re的涩图
-用法：/yande [tag]"""
 
 yande = on_command("yande", priority=10)
 danbooru = on_command("danbooru", priority=10)
+konachan = on_command("konachan", priority=10)
 
 
 @yande.handle()
-async def yande(bot: Bot, event: Event, args: Message = CommandArg()):
+async def yande_handle(bot: Bot, event: Event, args: Message = CommandArg()):
     url = 'https://yande.re/post.json?tags='+str(args).strip()
     img_list = json.loads(await httpx_request(url))
     img_url = choice(img_list)['file_url']
@@ -23,8 +21,16 @@ async def yande(bot: Bot, event: Event, args: Message = CommandArg()):
 
 
 @danbooru.handle()
-async def danbooru(bot: Bot, event: Event, args: Message = CommandArg()):
+async def danbooru_handle(bot: Bot, event: Event, args: Message = CommandArg()):
     url = 'https://danbooru.donmai.us/posts.json?tags='+str(args).strip()
     img_list = json.loads(await httpx_request(url))
     img_url = choice(img_list)['file_url']
     await danbooru.finish(MessageSegment.image(img_url))
+
+
+@konachan.handle()
+async def konachan_handle(bot: Bot, event: Event, args: Message = CommandArg()):
+    url = 'https://konachan.com/post.json?tags='+str(args).strip()
+    img_list = json.loads(await httpx_request(url))
+    img_url = choice(img_list)['file_url']
+    await konachan.finish(MessageSegment.image(img_url))
