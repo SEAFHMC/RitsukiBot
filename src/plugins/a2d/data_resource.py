cbc88c52b1d9929747efa2c02611fb800bec9ba0
Lyn DK <d1366160279@gmail.com>
2022-02-16 21:00:42 +0800

    Update data_resource.py
    改用异步方法

--- src/plugins/a2d/data_resource.py
+++ src/plugins/a2d/data_resource.py
@@ -1,18 +1,20 @@
-from PicImageSearch import Ascii2D
+from PicImageSearch import NetWork, AsyncAscii2D
 
 
 async def a2d_func(imgs):
     # Using Color
-    ascii2d = Ascii2D(bovw=False)
-    res = ascii2d.search(imgs[0])
+    async with NetWork() as client:
+        ascii2d = AsyncAscii2D(client=client, bovw=False)
+        res = await ascii2d.search(imgs[0])
     result_color = {
         'thumbnail': res.raw[1].thumbnail,
         'title': res.raw[1].title, 'authors': res.raw[1].authors,
         'url': res.raw[1].url}
 
     # Using Bovm
-    ascii2d = Ascii2D()
-    res = ascii2d.search(imgs[0])
+    async with NetWork() as client:
+        ascii2d = AsyncAscii2D(client=client)
+        res = await ascii2d.search(imgs[0])
     result_bovm = {
         'thumbnail': res.raw[1].thumbnail,
         'title': res.raw[1].title, 'authors': res.raw[1].authors,
