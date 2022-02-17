9b948c2a224c0b301e7b34c5cee790c406c828af
Lyn DK <d1366160279@gmail.com>
2022-02-18 00:01:49 +0800

    Update __init__.py

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -1,10 +1,12 @@
-from nonebot.adapters.onebot.v11 import MessageSegment
+from email import message
+from nonebot.adapters.onebot.v11 import MessageSegment, GroupMessageEvent
 from nonebot.adapters import Bot, Event, Message
 from nonebot.params import CommandArg
 from nonebot import on_command
 import ujson as json
 from utils.utils import httpx_request
 from random import choice
+from .data_resource import msg_recall
 
 
 yande = on_command("yande", priority=10)
@@ -17,7 +19,11 @@ async def yande_handle(bot: Bot, event: Event, args: Message = CommandArg()):
     url = 'https://yande.re/post.json?tags='+str(args).strip()
     img_list = json.loads(await httpx_request(url))
     img_url = choice(img_list)['file_url']
-    await yande.finish(MessageSegment.image(img_url))
+    if isinstance(event, GroupMessageEvent):
+        res = await bot.send_group_msg(group_id=event.group_id, message=MessageSegment.image(img_url))
+        await msg_recall(res['message_id'])
+    else:
+        await yande.finish(MessageSegment.image(img_url))
 
 
 @danbooru.handle()
@@ -25,7 +31,11 @@ async def danbooru_handle(bot: Bot, event: Event, args: Message = CommandArg()):
     url = 'https://danbooru.donmai.us/posts.json?tags='+str(args).strip()
     img_list = json.loads(await httpx_request(url))
     img_url = choice(img_list)['file_url']
-    await danbooru.finish(MessageSegment.image(img_url))
+    if isinstance(event, GroupMessageEvent):
+        res = await bot.send_group_msg(group_id=event.group_id, message=MessageSegment.image(img_url))
+        await msg_recall(res['message_id'])
+    else:
+        await danbooru.finish(MessageSegment.image(img_url))
 
 
 @konachan.handle()
@@ -33,4 +43,8 @@ async def konachan_handle(bot: Bot, event: Event, args: Message = CommandArg()):
     url = 'https://konachan.com/post.json?tags='+str(args).strip()
     img_list = json.loads(await httpx_request(url))
     img_url = choice(img_list)['file_url']
-    await konachan.finish(MessageSegment.image(img_url))
+    if isinstance(event, GroupMessageEvent):
+        res = await bot.send_group_msg(group_id=event.group_id, message=MessageSegment.image(img_url))
+        await msg_recall(res['message_id'])
+    else:
+        await konachan.finish(MessageSegment.image(img_url))
