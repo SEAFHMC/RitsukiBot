bee7a1ae9dcc95dbbd308341964c5891d1cb05eb
Lyn DK <d1366160279@gmail.com>
2022-02-18 17:27:44 +0800

    Create config.py

---
+++ src/plugins/epic/config.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Extra
+from typing import List
+
+class Config(BaseModel, extra=Extra.ignore):
+    epic_subscribe_group: List[int]
+    epic_subscribe_user: List[int]
