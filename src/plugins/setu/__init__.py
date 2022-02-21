40ef7174711835f48b7fab388f40f829d1b4ff98
Lyn DK <d1366160279@gmail.com>
2022-02-21 21:38:06 +0800

    Update __init__.py

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -3,7 +3,7 @@ from nonebot.adapters import Bot, Event, Message
 from nonebot.params import CommandArg
 from nonebot import on_command
 import ujson as json
-from utils.utils import httpx_request
+from utils.utils import httpx_get
 from random import choice
 from .data_resource import msg_recall
 
@@ -16,7 +16,7 @@ konachan = on_command("konachan", priority=10)
 @yande.handle()
 async def yande_handle(bot: Bot, event: Event, args: Message = CommandArg()):
     url = 'https://yande.re/post.json?tags='+str(args).strip()
-    img_list = json.loads(await httpx_request(url))
+    img_list = json.loads(await httpx_get(url))
     img_url = choice(img_list)['file_url']
     if isinstance(event, GroupMessageEvent):
         res = await bot.send_group_msg(group_id=event.group_id, message=MessageSegment.image(img_url))
@@ -28,7 +28,7 @@ async def yande_handle(bot: Bot, event: Event, args: Message = CommandArg()):
 @danbooru.handle()
 async def danbooru_handle(bot: Bot, event: Event, args: Message = CommandArg()):
     url = 'https://danbooru.donmai.us/posts.json?tags='+str(args).strip()
-    img_list = json.loads(await httpx_request(url))
+    img_list = json.loads(await httpx_get(url))
     img_url = choice(img_list)['file_url']
     if isinstance(event, GroupMessageEvent):
         res = await bot.send_group_msg(group_id=event.group_id, message=MessageSegment.image(img_url))
@@ -40,7 +40,7 @@ async def danbooru_handle(bot: Bot, event: Event, args: Message = CommandArg()):
 @konachan.handle()
 async def konachan_handle(bot: Bot, event: Event, args: Message = CommandArg()):
     url = 'https://konachan.com/post.json?tags='+str(args).strip()
-    img_list = json.loads(await httpx_request(url))
+    img_list = json.loads(await httpx_get(url))
     img_url = choice(img_list)['file_url']
     if isinstance(event, GroupMessageEvent):
         res = await bot.send_group_msg(group_id=event.group_id, message=MessageSegment.image(img_url))
