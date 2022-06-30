52b3917c93698f04d0c485157198d4122b75b634
SEAFHMC <d1366160279@gmail.com>
2022-06-30 19:23:33 +0800

    1

--- src/plugins/setus.py
+++ src/plugins/setus.py
@@ -1,5 +1,6 @@
 from nonebot.plugin import on_command
 from nonebot.adapters.onebot.v11.message import MessageSegment, Message
+from nonebot.adapters.onebot.v11.event import MessageEvent, PrivateMessageEvent
 from nonebot.params import CommandArg
 from httpx import AsyncClient
 
@@ -8,11 +9,31 @@ setu = on_command("setu", aliases={"涩图", "色图", "瑟图"})
 
 
 @setu.handle()
-async def _(arg:Message=CommandArg()):
+async def _(event: MessageEvent, arg: Message = CommandArg()):
     if not arg:
-        await setu.finish(MessageSegment.image("http://127.0.0.1:61658/setu/random"))
+        await setu.finish(
+            MessageSegment.image("http://127.0.0.1:61658/setu/random?r18=0")
+        )
+    if arg.extract_plain_text() == "r18" and isinstance(event, PrivateMessageEvent):
+        await setu.finish(
+            MessageSegment.image("http://127.0.0.1:61658/setu/random?r18=1")
+        )
+    args = arg.extract_plain_text().split()
     async with AsyncClient() as client:
-        resp = await client.get(f"http://127.0.0.1:61658/setu/search?tag={arg.extract_plain_text()}")
+        if args[0] == "r18" and isinstance(event, PrivateMessageEvent):
+            resp = await client.get(
+                f"http://127.0.0.1:61658/setu/search?tag={args[1]}&r18=1"
+            )
+        else:
+            resp = await client.get(
+                f"http://127.0.0.1:61658/setu/search?tag={args[0]}&r18=0"
+            )
     if resp.json()["code"] == 200:
-        await setu.finish(MessageSegment.image(resp.json()["img_url"]))
+        await setu.finish(
+            MessageSegment.image(
+                resp.json()["img_url"].replace(
+                    "https://i.pximg.net/", "http://127.0.0.1:17777/pixiv/"
+                )
+            )
+        )
     await setu.finish(resp.json()["message"])
