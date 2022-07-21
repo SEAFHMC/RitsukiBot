85425e39b6c27119541afd0c8786caa92fd2bf4c
SEAFHMC <d1366160279@gmail.com>
2022-07-21 16:34:50 +0800

    涩图！

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -11,7 +11,7 @@ setu = on_regex(r"来([0123456789]*)[份张]([rR]18)?(.*)的?[涩色瑟]图")
 
 @setu.handle()
 async def _(event: MessageEvent, args=RegexGroup()):
-    number = args[0] or 1
+    number = int(args[0]) if args[0].isdigit() else 1
     tag = args[2] or '"'
     if number > 10:
         await setu.finish("最多10张哦")
