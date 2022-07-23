bb35d056ed7ee21d56a71a4f730c05374cd7b9ce
SEAFHMC <d1366160279@gmail.com>
2022-07-23 09:34:17 +0800

    涩图！

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -1,6 +1,7 @@
 from nonebot.plugin import on_regex
 from nonebot.adapters.onebot.v11.message import MessageSegment
 from nonebot.adapters.onebot.v11.event import MessageEvent
+from nonebot.adapters.onebot.v11.bot import Bot
 from nonebot.params import RegexGroup
 from .get_setu import Setu
 from .anti_river_crab import enhanced_setu
@@ -10,17 +11,28 @@ setu = on_regex(r"来([0123456789]*)[份张]([rR]18)?(.*)的?[涩色瑟]图")
 
 
 @setu.handle()
-async def _(event: MessageEvent, args=RegexGroup()):
+async def _(bot: Bot, args=RegexGroup()):
     number = int(args[0]) if args[0].isdigit() else 1
     tag = args[2] or '"'
     if number > 10:
         await setu.finish("最多10张哦")
     resp = await Setu.get_setu(tag=tag, r18=0, number=number)
     if resp.code == 200:
-        msg = MessageSegment.reply(event.message_id)
+        msg = []
         try:
             for i in resp.data:
-                msg += MessageSegment.image(await enhanced_setu(url=i.img_url, pid=i.pid))
+                msg.append(
+                    MessageSegment(
+                        "node",
+                        {
+                            "uin": bot.self_id,
+                            "name": "Bot",
+                            "content": MessageSegment.image(
+                                await enhanced_setu(url=i.img_url, pid=i.pid)
+                            ),
+                        },
+                    )
+                )
         except Exception:
             pass
         await setu.finish(msg)
