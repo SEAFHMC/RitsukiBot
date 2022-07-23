e3884784465b34722c1243705b33a71577862a42
SEAFHMC <d1366160279@gmail.com>
2022-07-23 09:44:09 +0800

    合并转发涩图！

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -1,7 +1,8 @@
 from nonebot.plugin import on_regex
 from nonebot.adapters.onebot.v11.message import MessageSegment
-from nonebot.adapters.onebot.v11.event import MessageEvent
+from nonebot.adapters.onebot.v11.event import GroupMessageEvent, MessageEvent
 from nonebot.adapters.onebot.v11.bot import Bot
+from nonebot.exception import FinishedException
 from nonebot.params import RegexGroup
 from .get_setu import Setu
 from .anti_river_crab import enhanced_setu
@@ -11,7 +12,7 @@ setu = on_regex(r"来([0123456789]*)[份张]([rR]18)?(.*)的?[涩色瑟]图")
 
 
 @setu.handle()
-async def _(bot: Bot, args=RegexGroup()):
+async def _(bot: Bot, event: MessageEvent, args=RegexGroup()):
     number = int(args[0]) if args[0].isdigit() else 1
     tag = args[2] or '"'
     if number > 10:
@@ -35,5 +36,9 @@ async def _(bot: Bot, args=RegexGroup()):
                 )
         except Exception:
             pass
-        await setu.finish(msg)
+        if isinstance(event, GroupMessageEvent):
+            await bot.send_group_forward_msg(group_id=event.group_id, messages=msg)
+        else:
+            await bot.send_private_forward_msg(user_id=event.user_id, messages=msg)
+        raise FinishedException
     await setu.finish(resp.message)
