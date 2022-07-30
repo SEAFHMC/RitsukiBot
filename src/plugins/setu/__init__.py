bafc64e748f68ebecacde5a814d73accce05d4d8
SEAFHMC <d1366160279@gmail.com>
2022-07-30 18:51:23 +0800

    合并转发会风控！

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -1,44 +1,33 @@
 from nonebot.plugin import on_regex
 from nonebot.adapters.onebot.v11.message import MessageSegment
-from nonebot.adapters.onebot.v11.event import GroupMessageEvent, MessageEvent
-from nonebot.adapters.onebot.v11.bot import Bot
 from nonebot.exception import FinishedException
 from nonebot.params import RegexGroup
 from .get_setu import Setu
 from .anti_river_crab import enhanced_setu
+from asyncio import sleep as asleep
 
 
 setu = on_regex(r"来([0123456789]*)[份张]([rR]18)?(.*)的?[涩色瑟]图")
 
 
 @setu.handle()
-async def _(bot: Bot, event: MessageEvent, args=RegexGroup()):
+async def _(args=RegexGroup()):
     number = int(args[0]) if args[0].isdigit() else 1
     tag = args[2] or '"'
     if number > 10:
         await setu.finish("最多10张哦")
     resp = await Setu.get_setu(tag=tag, r18=0, number=number)
     if resp.code == 200:
-        msg = []
+        msg_list = []
         try:
             for i in resp.data:
-                msg.append(
-                    MessageSegment(
-                        "node",
-                        {
-                            "uin": event.user_id,
-                            "name": event.sender.card or event.sender.nickname,
-                            "content": MessageSegment.image(
-                                await enhanced_setu(url=i.img_url, pid=i.pid)
-                            ),
-                        },
-                    )
+                msg_list.append(
+                    MessageSegment.image(await enhanced_setu(url=i.img_url, pid=i.pid))
                 )
         except Exception:
             pass
-        if isinstance(event, GroupMessageEvent):
-            await bot.send_group_forward_msg(group_id=event.group_id, messages=msg)
-        else:
-            await bot.send_private_forward_msg(user_id=event.user_id, messages=msg)
+        for msg in msg_list:
+            await setu.send(msg)
+            await asleep(2)
         raise FinishedException
     await setu.finish(resp.message)
