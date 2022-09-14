6ab9a8d267e0be089c4afe72169f15b7197988be
SEAFHMC <d1366160279@gmail.com>
2022-09-14 10:14:17 +0800

    update bot.py

--- RitsukiBot/bot.py
+++ RitsukiBot/bot.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import nonebot
+from nonebot.log import logger
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
 # Custom your logger
@@ -32,7 +33,5 @@ nonebot.load_from_toml("pyproject.toml")
 
 
 if __name__ == "__main__":
-    nonebot.logger.warning(
-        "Always use `nb run` to start the bot instead of manually running!"
-    )
+    logger.warning("Always use `nb run` to start the bot instead of manually running!")
     nonebot.run(app="__mp_main__:app")
