0dfce19a392cf5f89483fb678a048f4e3f7d1c38
SEAFHMC <d1366160279@gmail.com>
2022-10-13 11:30:21 +0800

    update wordcloud

--- RitsukiBot/src/plugins/wordcloud/__init__.py
+++ RitsukiBot/src/plugins/wordcloud/__init__.py
@@ -4,6 +4,7 @@ from nonebot.adapters.onebot.v11 import GroupMessageEvent, MessageSegment
 from time import time
 from .data_source import enable_group, format_time
 from .chat_recorder import ChatRecorder
+from .config import plugin_config
 
 monitor = on_message(rule=enable_group, block=False)
 
@@ -23,10 +24,12 @@ generate = on_command("生成今日词云", aliases={"生成词云"})
 
 @generate.handle()
 async def _(event: GroupMessageEvent):
-    await generate.finish(
-        MessageSegment.image(
-            ChatRecorder.generate_wordcloud(
-                date=format_time(time()), group_id=event.group_id
+    if event.group_id in plugin_config.wordcloud_enable_group:
+        await generate.finish(
+            MessageSegment.image(
+                ChatRecorder.generate_wordcloud(
+                    date=format_time(time()), group_id=event.group_id
+                )
             )
         )
-    )
+    await generate.finish("该群未启用词云")
